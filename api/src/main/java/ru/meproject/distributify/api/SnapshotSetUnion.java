a6481e47ebc21ccf95c3d6c5d90a50669561fc8d
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2023-07-10 01:07:12 +0600

    fix: delegating Expiry behaviour to JedisDriverConfig

---
+++ api/src/main/java/ru/meproject/distributify/api/SnapshotSetUnion.java
@@ -0,0 +1,33 @@
+package ru.meproject.distributify.api;
+
+import java.util.Set;
+
+/**
+ * SnapshotSetUnion is for creating union of sets that can perform operations on it.
+ * Each instance keeps track of data points in its own set. The union of said sets is set of all data points in a system.
+ * The lowest data point in this structure must be a set. Meaning we operate with subsets only.
+ * Also meaning we don't support adding and discarding players one by one.
+ * The snapshot of instance set should be taken at fixed rate (or at some event if you want) and sent to the structure.
+ * Implementation directives:
+ * 1. If it's allowed for underlying driver, we should use transactional operations (ACID, reliable operations) to avoid cases when instance tries to read set union and one of subsets is missing because of its snapshot being "in-progress"
+ * 2. If it's allowed for underlying driver, we should use expiry functionality or create clean up task (with heartbeat pattern or driver-side task) for cases where instance is down in irregular manner (e.g was killed) and its set was not snapshotted for a while.
+ * Examples:
+ * Cases like whole system player list.
+ */
+public interface SnapshotSetUnion<E> {
+
+    /**
+     * Send snapshot of a subset to database
+     * @param set - set of values to be sent
+     */
+    void snapshotSubSet(Set<E> set);
+
+    /**
+     * Get latest subset from database
+     * @return
+     */
+    Set<E> getSubSet();
+
+    Set<E> getUnionSet();
+
+}
